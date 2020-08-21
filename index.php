66cff2affc9c625dca0984ff929fa498969533ff
Danny <70016750+NoobDerInformatik@users.noreply.github.com>
2020-08-21 15:38:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Das ist ein Test
+<?php
+$klartext = "geheim";
+$hash = password_hash($klartext, PASSWORD_BCRYPT);
+echo $hash."<br>";
+
+if (password_verify('geheim', $hash)) {
+    echo "pw stimmt";
+}else{
+    echo "pw stimmt nicht";
+}
+?>
